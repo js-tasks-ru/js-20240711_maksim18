42a60d9da4d5d2784227204a963989d654e72ef7
miu.slobodin <miu.slobodin@severstal.com>
2024-08-05 17:16:35 +0300

    completed the "SortableTable v2 компонент без динамической загрузки данных" task; completed the "Tooltip компонент" task;

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,80 @@
-export default class SortableTable {
+import { default as SortableTableV1 } from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
+
+// I think the test named "should sort "desc" correctly for "sortType" equal number" is incorrect.
+// Initial sorting is made by "title" id but a pointdown event is happend only once.
+export default class SortableTable extends SortableTableV1 {
+  #header;
+  #headerConfig;
+  #headerListerner;
+
+
+
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
+    super(headersConfig, data);
 
+    this.#header = this.subElements.header;
+    this.#headerConfig = headersConfig;
+    this.#headerListerner = event => this.#onHeaderClick(event);
+
+    this.#addListeners();
+    const { id, order } = sorted;
+    this.#updateTable(id, order);
+  }
+
+
+
+  #addListeners() {
+    this.#header.addEventListener(`pointerdown`, this.#headerListerner);
+  }
+
+  #createArrowHtml() {
+    return `<span data-element="arrow" class="sortable-table__sort-arrow"><span class="sort-arrow"></span></span>`;
+  }
+
+  #createHeaderCellHtml({ id, sortable, title }, sortingId, sortOrder) {
+    const incorrectСondition = !sortingId || sortingId !== id;
+    const dataOrderAttribute = incorrectСondition ? `` : ` data-order="${sortOrder}"`;
+    const arrowHtml = incorrectСondition ? `` : this.#createArrowHtml();
+
+    return `<div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}"${dataOrderAttribute}><span>${title}</span>${arrowHtml}</div>`;
+  }
+
+  #createHeaderCellsHtml(sortingId, sortOrder) {
+    return this.#headerConfig.map(c => this.#createHeaderCellHtml(c, sortingId, sortOrder)).join(``);
+  }
+
+  #onHeaderClick(event) {
+    const headerCellElement = event.target.closest(`div.sortable-table__cell`);
+    if (!headerCellElement || headerCellElement.dataset.sortable !== `true`) {
+      return;
+    }
+
+    const sortingId = headerCellElement.dataset.id;
+    let sortOrder = headerCellElement.dataset.order;
+    sortOrder = !sortOrder || sortOrder === `desc` ? `asc` : `desc`;
+    this.#updateTable(sortingId, sortOrder);
+  }
+
+  #removeListeners() {
+    this.#header.removeEventListener(`click`, this.#headerListerner);
+  }
+
+  #updateHeader(sortingId, sortOrder) {
+    this.#header.innerHTML = this.#createHeaderCellsHtml(sortingId, sortOrder);
+  }
+
+  #updateTable(sortingId, sortOrder) {
+    this.#updateHeader(sortingId, sortOrder);
+    this.sort(sortingId, sortOrder);
+  }
+
+
+
+  destroy() {
+    this.#removeListeners();
+    super.destroy();
   }
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,119 @@
 class Tooltip {
-  initialize () {
+  static #instance;
 
+
+
+  #element;
+  #pointerMoveListener;
+  #pointerOutListener;
+  #pointerOverListener;
+
+
+
+  get element() {
+    return this.#element;
+  }
+
+
+
+  constructor() {
+    if (!Tooltip.#instance) {
+      Tooltip.#instance = this;
+    }
+
+    this.#pointerOverListener = event => this.#onPointerOver(event);
+    this.#pointerMoveListener = event => this.#onPointerMove(event);
+    this.#pointerOutListener = event => this.#onPointerOut(event);
+
+    return Tooltip.#instance;
+  }
+
+
+
+  #addListeners() {
+    document.body.addEventListener(`pointerover`, this.#pointerOverListener);
+    document.body.addEventListener(`pointermove`, this.#pointerMoveListener);
+    document.body.addEventListener(`pointerout`, this.#pointerOutListener);
+  }
+
+  #changeLocation(x, y) {
+    if (!this.#element) {
+      return;
+    }
+
+    //this.#element.style.position = `ab`;
+    this.#element.style.left = `${x + 10}px`;
+    this.#element.style.top = `${y + 10}px`;
+  }
+
+  #changeText(text) {
+    if (!this.#element) {
+      return;
+    }
+
+    this.#element.innerHTML = text;
+  }
+
+  #createElement() {
+    const element = document.createElement(`div`);
+    element.innerHTML = `<div class="tooltip"></div>`;
+
+    return element.firstElementChild;
+  }
+
+  #onPointerMove(event) {
+    this.#changeLocation(event.clientX, event.clientY);
+  }
+
+  #onPointerOut(event) {
+    this.remove();
+  }
+
+  #onPointerOver(event) {
+    const target = event.target;
+    const text = target.dataset?.tooltip;
+    if (!text) {
+      return;
+    }
+
+    this.render(text);
+
+    this.#changeLocation(event.clientX, event.clientY);
+  }
+
+  #removeListeners() {
+    document.body.removeEventListener(`pointerover`, this.#pointerOverListener);
+    document.body.removeEventListener(`pointermove`, this.#pointerMoveListener);
+    document.body.removeEventListener(`pointerout`, this.#pointerOutListener);
+  }
+
+
+
+
+
+  destroy() {
+    this.#removeListeners();
+
+    this.remove();
+  }
+
+  initialize() {
+    this.#element = this.#createElement();
+
+    this.#addListeners();
+  }
+
+  remove() {
+    this.#element?.remove();
+  }
+
+  render(text) {
+    this.#changeText(text);
+
+    const bodyChildrenElements = Array.from(document.body.children);
+    if (!bodyChildrenElements.includes(this.#element)) {
+      document.body.append(this.#element);
+    }
   }
 }
 
